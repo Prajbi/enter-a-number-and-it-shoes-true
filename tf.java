dfb149a076bbdcf128c631eb214375cda8669a89
Prajwal Biradar <112881872+Prajbi@users.noreply.github.com>
2024-09-28 19:10:04 +0530

    Add files via upload

---
+++ tf.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class tf {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Boolean b = sc.nextBoolean();
+        System.out.println("enter number");
+        Boolean n = true;
+        System.out.println("it is " + n);
+    }
+}
